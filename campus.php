47ec2990fc0ab93fb6ef34c3b1a58650df3c9fba
fahrun-nisa <71806634+fahrun-nisa@users.noreply.github.com>
2020-09-26 18:23:49 +0700

    Update campus.php

--- campus.php
+++ campus.php
@@ -1,21 +1,43 @@
-<p>Fasilities at Syarif Hidayatullah State Islamic University Jakarta</p align="center">
-<img src="uin.jpeg" width="300px" height="200px" align="center">
-<p>Landmark UIN Jakarta</p>
-<img src="harun.jpeg" width="300px" height="200px" align="center">
-<p>auditorium Harun Nasution UIN Jakarta</p>
-<img src="msjid.jpeg" width="300px" height="200px" align="center">
-<p>Masjid al-jamiah UIN Jakarta</p>
-<img src="sc.jpg" width="300px" height="200px" align="center">
-<p>Student Center UIN Jakarta</p>
-<img src="cngkir.jpg" width="300px" height="200px" align="center">
-<p>Cafe cangkir UIN Jakarta</p>
-<img src="plt.jpg" width="300px" height="200px" align="center">
-<p>Pusat Labotorium Terpadu UIN Jakarta</p>
-<img src="perpus.jpg" width="300px" height="200px" align="center">
-<p>Perpustakaan UIN Jakarta</p>
-<img src="syhid.jpg" width="300px" height="200px" align="center">
-<p>Syahid Inn</p>
-<img src="pustiknas.jpg" width="300px" height="200px" align="center">
-<p>Pusat TIK Nasional</p>
-<img src="bhs.jpg" width="300px" height="200px" align="center">
-<p>Pusat Bahasa</p>
+<h2>Fasilities at Syarif Hidayatullah State Islamic University Jakarta</h2 align="center">
+
+<img src="uin.jpeg" width="50%" height="50%" style="display:block;margin:auto;">
+<b><font yang size="5"><p style="text-align:center"> Landmark</b></font></p>
+<small>The characteristic of Syarif Hidayatullah State Islamic University Jakarta which is usually crowded with students to take pictures and usually new prospective students will always visit and take the time to take pictures here</small>
+
+<img src="harun.jpeg" width="50%" height="50%" style="display:block;margin:auto;">
+<b><font yang size="5"><p style="text-align:center">Auditorium Harun Nasution</b></font></p>
+<small>Harun Nasution Auditorium which consists of two floors, can accommodate about a thousand people. Apart from that, the Harun Nasution Auditorium is never devoid of the bustle of activities, such as seminars,workshops, and graduation processions.</small>
+
+<img src="msjid.jpeg" width="50%" height="50%" style="display:block;margin:auto;">
+<b><font yang size="5"><p style="text-align:center"> Al-Jami'ah Mosque</b></font></p>
+<small>The mosque or prayer space itself is on the second floor while the first floor is the SC hall which is a favorite place for students to hold activities. On the side of the hall (first floor) this is a large ablution place with supporting facilities.</small>
+
+<img src="sc.jpg" width="50%" height="50%" style="display:block;margin:auto;">
+<b><font yang size="5"><p style="text-align:center"> Student Center</b></font></p>
+<small>The Student Center is a large building, which includes a mini stadium and its field, a mosque, a hall, a cooperative, and a UKM secretariat. There are also various choices of UKM in UIN Jakarta, according to their interests and talents.</small>
+
+<img src="cngkir.jpg" width="50%" height="50%" style="display:block;margin:auto;">
+<b><font yang size="5"><p style="text-align:center"> Cup Cafe</b></font></p>
+<small>This giant cup-shaped cafe provides various kinds of menus such as various rice, various noodle dishes, chicken, fish, side dishes, and others. Apart from food, Cup Café is also equipped with a mini-market that sells complete student needs, from stationery to household needs.</small>
+
+<img src="plt.jpg" width="50%" height="50%" style="display:block;margin:auto;">
+<b><font yang size="5"><p style="text-align:center"> Integrated Laboratory Center</b></font></p>
+<small>he Center for Integrated Laboratory of Syarif Hidayatullah State Islamic University Jakarta or abbreviated as PLT-UIN Syarif Hidayatullah Jakarta was founded in 2003 in line with the opening of the Faculty of Science and Technology (FST-UIN Syarif Hidayatullah Jakarta). PLT-UIN Syarif Hidayatullah Jakarta is prepared to hold practicum, so that in addition to being equipped with theoretical knowledge in college, every student is also provided with practical skills, so that scholars can produce academically qualified and have competitive and professional skills.</small>
+
+<img src="perpus.jpg" width="50%" height="50%" style="display:block;margin:auto;">
+<b><font yang size="5"><p style="text-align:center"> Main Library</b></font></p>
+<small>A library is a building used for storage and use of books. Libraries are also a storehouse of knowledge, where there are many types of books. Every student, student or public can use the library to borrow books and space to read. This library is very comfortable because it is equipped with many facilities such as free internet and lots of reading places.</small>
+
+<img src="syhid.jpg" width="50%" height="50%" style="display:block;margin:auto;">
+<b><font yang size="5"><p style="text-align:center"> Syahida Inn</b></font></p>
+<small>Syahida Inn is a matter of pride because anyone can access it for workshops, seminars, grand meetings, and even wedding receptions. Syahida Inn consists of six floors with VIP bedroom facilities, courtroom, hall, canteen.</small>
+
+<img src="pustiknas.jpg" width="50%" height="50%" style="display:block;margin:auto;">
+<b><font yang size="5"><p style="text-align:center"> National ICT Center</b></font></p>
+<small>The National ICT Center (Pustiknas) is a transformation of the National Information and Communication Technology for Human Resources Development (NICT-HRD) Project which is managed under the Human Resources Research and Development Agency of the Ministry of Communication and Information Technology of the Republic of Indonesia in collaboration with Syarif Hidayatullah State Islamic University Jakarta.</small>
+
+<img src="bhs.jpg" width="50%" height="50%" style="display:block;margin:auto;">
+<b><font yang size="5"><p style="text-align:center"> Language Development Center</b></font></p>
+<small>The Language Center was founded in 1970. It was originally intended to carry out language processes, especially Arabic and English. Along with the process of continuous improvement and learning, in subsequent developments this institution was given the confidence to hold the language test. Over time, UIN will become an International Research University. Among the main activities of international universities are academic relations, exchange of teaching staff, student communication, and research collaboration between countries. That means communicating in writing and orally in international languages ​​is a must.
+Accordingly, in 2005, the Chancellor of UIN issued a Decree: the standard score for Arabic (Test of Arabic as a Foreign Language) and English (English Competency Test) for undergraduate scholars is 450-475; S2 and S 3, 500.
+The Language Center tries to focus on aspects of international language learning, while the existing faculties focus more on management and research in their respective disciplines.</small>
