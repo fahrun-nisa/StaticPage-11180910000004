2513b9a09b87576782409deb7fed7474e7ebb528
fahrun-nisa <71806634+fahrun-nisa@users.noreply.github.com>
2020-09-26 18:20:20 +0700

    Update home.php

--- home.php
+++ home.php
@@ -1,3 +1,3 @@
-<h2>Hello </h2> 
-<p>My name is Fahrunnisa. Call me is Nisa. I was born in Jakarta in 2000.</p>
-<p>I'am  student at the Syarif Hidayatullah State Islamic University Jakarta. Faculty of Science and Technology, Informatics Engineering Study Program.</p>
+<h2>Hello</h2> 
+<p>My name is Fahrunnisa. Call me is Nisa. I was born in Jakarta in 2000.
+I'am  student at the Syarif Hidayatullah State Islamic University Jakarta. Faculty of Science and Technology, Informatics Engineering Study Program.</p>
