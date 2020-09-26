48b3c16682fbe3e70c1b66e6bed2f95c70c021a4
fahrun-nisa <71806634+fahrun-nisa@users.noreply.github.com>
2020-09-26 10:38:10 +0700

    Update campus.php

--- campus.php
+++ campus.php
@@ -1,21 +1,21 @@
 <p>Fasilities at Syarif Hidayatullah State Islamic University Jakarta</p align="center">
-<img src="img/uin.jpeg" width="300px" height="200px" align="center">
+<img src="uin.jpeg" width="300px" height="200px" align="center">
 <p>Landmark UIN Jakarta</p>
-<img src="img/harun.jpeg" width="300px" height="200px" align="center">
+<img src="harun.jpeg" width="300px" height="200px" align="center">
 <p>auditorium Harun Nasution UIN Jakarta</p>
-<img src="img/msjid.jpeg" width="300px" height="200px" align="center">
+<img src="msjid.jpeg" width="300px" height="200px" align="center">
 <p>Masjid al-jamiah UIN Jakarta</p>
-<img src="img/sc.jpg" width="300px" height="200px" align="center">
+<img src="sc.jpg" width="300px" height="200px" align="center">
 <p>Student Center UIN Jakarta</p>
-<img src="img/cngkir.jpg" width="300px" height="200px" align="center">
+<img src="cngkir.jpg" width="300px" height="200px" align="center">
 <p>Cafe cangkir UIN Jakarta</p>
-<img src="img/plt.jpg" width="300px" height="200px" align="center">
+<img src="plt.jpg" width="300px" height="200px" align="center">
 <p>Pusat Labotorium Terpadu UIN Jakarta</p>
-<img src="img/perpus.jpg" width="300px" height="200px" align="center">
+<img src="perpus.jpg" width="300px" height="200px" align="center">
 <p>Perpustakaan UIN Jakarta</p>
-<img src="img/syhid.jpg" width="300px" height="200px" align="center">
+<img src="syhid.jpg" width="300px" height="200px" align="center">
 <p>Syahid Inn</p>
-<img src="img/pustiknas.jpg" width="300px" height="200px" align="center">
+<img src="pustiknas.jpg" width="300px" height="200px" align="center">
 <p>Pusat TIK Nasional</p>
-<img src="img/bhs.jpg" width="300px" height="200px" align="center">
+<img src="bhs.jpg" width="300px" height="200px" align="center">
 <p>Pusat Bahasa</p>
