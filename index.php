b67d720d8ea056487d3dcd4bab71c740eaa32bd0
fahrun-nisa <71806634+fahrun-nisa@users.noreply.github.com>
2020-09-26 10:36:13 +0700

    Update index.php

--- index.php
+++ index.php
@@ -4,22 +4,22 @@
 
 <head>
 	<title>Static Page</title>
-	<link href="style/style.css" type="text/css" rel="stylesheet" />
+	<link href="style.css" type="text/css" rel="stylesheet" />
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.6.0/Chart.min.js" type="text/javascript"></script>
 </head>
 
 <body>
 	<div id="container">
 		<div id="header">
-		<img src="img/uinjkt.png" width="150px" height="100px" align="left">
+		<img src="uinjkt.png" width="150px" height="100px" align="left">
 		<center>
 			<h1>Syarif Hidayatullah State Islamic University Jakarta</h1>
 			<p>Jl. Ir H. Juanda No.95, Cempaka Putih, Kecamatan Cempaka Timur, Kota Tangerang Selatan, Banten 15412</p>
 		</center>
 			<ul id="navmenu">
 				<li><a href="index.php" class="selected">Profil</a></li>
-				<li><a href="?module=campus#pos">Campus Life</a></li>
-				<li><a href="?module=about#pos">About</a></li>
+				<li><a href="campus.php">Campus Life</a></li>
+				<li><a href="about.php">About</a></li>
 			</ul>
 		</div>
 		
@@ -27,7 +27,7 @@
 					<?php if(isset ($_GET['module']))
 				include "konten/$_GET[module].php";
 			else 
-				include "konten/home.php";?> 
+				include "home.php";?> 
 			</div>
 			
 		<div id="clear"></div>
