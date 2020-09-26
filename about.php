9ec77b85f6c6881c6f4bf43b88b337964d8ee427
fahrun-nisa <71806634+fahrun-nisa@users.noreply.github.com>
2020-09-26 18:24:41 +0700

    Update about.php

--- about.php
+++ about.php
@@ -1,2 +1,2 @@
-<p>Fahrunnisa</p align="center">
-<img src="me.jpg" width="300px" height="350px" align="center">
+<h2>Fahrunnisa</h2 align="center">
+<img src="me.jpg" width="300px" height="350px" style="display:block;margin:auto;">
